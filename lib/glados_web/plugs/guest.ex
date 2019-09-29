0bcbc6d7b256e0e1dad526fe2299b7ac6489a34b
Håvard Pedersen <havardpede@gmail.com>
2019-09-29 19:42:52 +0100

    fix compile errors

--- lib/glados_web/plugs/guest.ex
+++ lib/glados_web/plugs/guest.ex
@@ -1,14 +1,14 @@
 defmodule GladosWeb.Plugs.Guest do
   import Plug.Conn
   import Phoenix.Controller
-  import GladosWeb.Router.Helpers
+  alias GladosWeb.Router.Helpers, as: Routes
 
   def init(opts), do: opts
 
   def call(conn, _opts) do
     if Plug.Conn.get_session(conn, :current_user_id) do
       conn
-      |> redirect(to: user_path(conn, :index))
+      |> redirect(to: Routes.user_path(conn, :index))
       |> halt()
     else
       conn
