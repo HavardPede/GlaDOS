acd687a8ca0fe7017cba5cc356f5c7b6d44c0bee
Håvard Pedersen <havardpede@gmail.com>
2020-02-15 20:35:09 +0000

    specify db env in github actions worflow

--- config/test.exs
+++ config/test.exs
@@ -2,16 +2,11 @@ use Mix.Config
 
 # Configure your database
 config :glados, Glados.Repo,
+  username: "postgres",
+  password: "postgres",
   database: "glados_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
-
-# Configure the database for GitHub Actions
-if System.get_env("GITHUB_ACTIONS") do
-  config :app, Glados.Repo,
-    username: "postgres",
-    password: "postgres"
-end
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
