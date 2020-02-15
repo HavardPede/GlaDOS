cb4e0d6993072dbf9e83c982fcfc5fe19233a917
Håvard Pedersen <havardpede@gmail.com>
2020-02-15 20:29:32 +0000

    change environment for  github actions

--- config/test.exs
+++ config/test.exs
@@ -2,11 +2,16 @@ use Mix.Config
 
 # Configure your database
 config :glados, Glados.Repo,
-  username: "postgres",
-  password: "postgres",
   database: "glados_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
+
+# Configure the database for GitHub Actions
+if System.get_env("GITHUB_ACTIONS") do
+  config :app, Glados.Repo,
+    username: "postgres",
+    password: "postgres"
+end
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
@@ -17,11 +22,5 @@ config :glados, GladosWeb.Endpoint,
 config :glados,
   password_encryption: Glados.Accounts.MockEncryption
 
-# Configure the database for GitHub Actions
-if System.get_env("GITHUB_ACTIONS") do
-  config :app, Glados.Repo,
-    username: "postgres",
-    password: "postgres"
-end
 # Print only warnings and errors during test
 config :logger, level: :warn
