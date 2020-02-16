336aa36c2236db31f0ef4a421e5ec5baa76c4f15
Håvard Pedersen <havardpede@gmail.com>
2020-02-16 16:47:08 +0000

    ensure correct routing when sending/updating crew application

--- lib/glados_web/router.ex
+++ lib/glados_web/router.ex
@@ -98,7 +98,7 @@ defmodule GladosWeb.Router do
     pipe_through [:browser, :member, :fetch_event]
 
     get("/forside", MemberController, :event_landing)
-    get("/søknad", MemberController, :crew_application)
+    get("/soknad", MemberController, :crew_application)
   end
 
   # Logger Scope
