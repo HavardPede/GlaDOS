12eaa344f365d01cfdfebe6d503faf0895dd74f7
Håvard Pedersen <havardpede@gmail.com>
2020-02-19 22:25:21 +0000

    add all domains that resolves to this url

--- config/prod.exs
+++ config/prod.exs
@@ -2,6 +2,7 @@ use Mix.Config
 
 config :glados, GladosWeb.Endpoint,
   url: [host: "eslgcrew.no", port: 443, scheme: "https"],
+  check_origin: ["https://eslgcrew.no", "https://www.eslgcrew.no"],
   cache_static_manifest: "priv/static/cache_manifest.json",
   static_url: [scheme: "https", host: "d2haayejdu10d3.cloudfront.net", port: 443],
   http: [port: {:system, "PORT"}],
